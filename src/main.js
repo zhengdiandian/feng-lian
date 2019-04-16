95048bcd26e375842a7db0bdca32fc5d0d4f2ebd
zhengdiandian <zzp19980703@gmail.com>
2019-04-16 09:41:29 +0800

    引入museUi, axios

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'muse-ui/lib/styles/base.less'
+import { Button, Select } from 'muse-ui'
+// import 'muse-ui/lib/styles/theme.less'
+import 'muse-ui/dist/muse-ui.css'
+
+Vue.use(Button)
+Vue.use(Select)
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,6 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+   <mu-button color="primary">Primary</mu-button>
+
 </template>
 
 <script>
