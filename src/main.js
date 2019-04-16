38ef025e6ad7fc7fb522aa6758c25efe3167e95b
zhengdiandian <zzp19980703@gmail.com>
2019-04-16 10:46:33 +0800

    定义全局宽度

--- src/main.js
+++ src/main.js
@@ -3,12 +3,38 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'muse-ui/lib/styles/base.less'
-import { Button, Select } from 'muse-ui'
-// import 'muse-ui/lib/styles/theme.less'
+import { Button, Select, BottomNav } from 'muse-ui'
+import 'muse-ui/lib/styles/theme.less'
 import 'muse-ui/dist/muse-ui.css'
+import theme from 'muse-ui/lib/theme'
+theme.add('teal', {
+  primary: '#2196f3',
+  secondary: '#ff4081',
+  success: '#4caf50',
+  warning: '#fdd835',
+  info: '#2196f3',
+  error: '#f44336',
+  track: '#bdbdbd',
+  text: {
+    primary: 'rgba(0, 0, 0, 0.87)',
+    secondary: 'gba(0, 0, 0, 0.54)',
+    alternate: '#fff',
+    disabled: 'rgba(0, 0, 0, 0.38)',
+    hint: 'rgba(0, 0, 0, 0.38)' // 提示文字颜色
+  },
+  divider: 'rgba(0, 0, 0, 0.12)',
+  background: {
+    paper: '#fff',
+    chip: '#e0e0e0',
+    default: '#fafafa'
+  }
+}, 'light')
+
+theme.use('teal')
 
 Vue.use(Button)
 Vue.use(Select)
+Vue.use(BottomNav)
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,22 @@
 <template>
-   <mu-button color="primary">Primary</mu-button>
-
+<mu-container style="max-width: 400px; width:100%;">
+  <mu-bottom-nav :value.sync="shift" shift>
+    <mu-bottom-nav-item value="movies" title="Movies" icon="ondemand_video"></mu-bottom-nav-item>
+    <mu-bottom-nav-item value="music" title="Music" icon="music_note"></mu-bottom-nav-item>
+    <mu-bottom-nav-item value="books" title="Books" icon="books"></mu-bottom-nav-item>
+    <mu-bottom-nav-item value="pictures" title="Pictures" icon="photo"></mu-bottom-nav-item>
+  </mu-bottom-nav>
+</mu-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
