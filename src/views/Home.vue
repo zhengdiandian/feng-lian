da61a4fa2d114deb2fd98e3c21580f50bea4be84
zhengdiandian <zzp19980703@gmail.com>
2019-04-16 11:06:33 +0800

    更改视窗宽度为350px

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
 <mu-container style="max-width: 400px; width:100%;">
-  <mu-bottom-nav :value.sync="shift" shift>
+  <mu-bottom-nav value.sync="movies'" shift>
     <mu-bottom-nav-item value="movies" title="Movies" icon="ondemand_video"></mu-bottom-nav-item>
     <mu-bottom-nav-item value="music" title="Music" icon="music_note"></mu-bottom-nav-item>
     <mu-bottom-nav-item value="books" title="Books" icon="books"></mu-bottom-nav-item>
