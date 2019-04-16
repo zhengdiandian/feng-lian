72de70273463324580afe891c7fd8b330cabd23c
zhengdiandian <zzp19980703@gmail.com>
2019-04-16 14:07:16 +0800

    添加导航栏

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,14 @@
 <template>
-<mu-container style="max-width: 400px; width:100%;">
-  <mu-bottom-nav value.sync="movies'" shift>
-    <mu-bottom-nav-item value="movies" title="Movies" icon="ondemand_video"></mu-bottom-nav-item>
-    <mu-bottom-nav-item value="music" title="Music" icon="music_note"></mu-bottom-nav-item>
-    <mu-bottom-nav-item value="books" title="Books" icon="books"></mu-bottom-nav-item>
-    <mu-bottom-nav-item value="pictures" title="Pictures" icon="photo"></mu-bottom-nav-item>
-  </mu-bottom-nav>
-</mu-container>
+  <div>
+    <router-view></router-view>
+    <mu-container style="max-width: 400px; width:100%;">
+      <mu-bottom-nav :value.sync="shift" shift>
+        <mu-bottom-nav-item value="movies" title="首页" icon="home"></mu-bottom-nav-item>
+        <mu-bottom-nav-item value="music" title="公告" icon="assignment"></mu-bottom-nav-item>
+        <mu-bottom-nav-item value="books" title="我的"  icon="person"></mu-bottom-nav-item>
+      </mu-bottom-nav>
+    </mu-container>
+  </div>
 </template>
 
 <script>
@@ -15,6 +17,11 @@
 
 export default {
   name: 'home',
+  data() {
+    return {
+      shift: 'movies'
+    }
+  },
   components: {
     
   }
