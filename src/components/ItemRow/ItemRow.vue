aed7d232d3cdcab1d7979739251be1ad7e3f4266
zhengdiandian <zzp19980703@gmail.com>
2019-04-18 18:12:02 +0800

    互助项目完成

---
+++ src/components/ItemRow/ItemRow.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="item-row">
+      <div class="item" v-for="(data,i) in datas" :key="i">
+        <span :class="'iconfont '+ data.icon"  class="item-row-icon"></span>
+        <div class="title">{{data.title}}</div>
+        <div class="text">{{data.text}}</div>
+      </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'ItemRow.vue',
+    props: {
+      datas: {
+        type: Array,
+        default: function() {
+          return [ {
+            title: '互助范围',
+            text: '60 (大病) + 20(轻症)'
+          }]
+        },
+
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+  .item-row{
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    &-icon{
+      /*width: 50px;*/
+      /*height: 50px;*/
+      font-size: 50px;
+
+    }
+    /*flex-wrap: wrap;*/
+  }
+  .item{
+    width: 25%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    .text{
+      /*text-align: center;*/
+
+    }
+  }
+
+</style>
