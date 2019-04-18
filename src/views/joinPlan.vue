e9328d101ee38cdea3178fab5b536b424d705253
zhengdiandian <zzp19980703@gmail.com>
2019-04-18 21:21:42 +0800

    修改了joinPlan.vue

--- src/views/joinPlan.vue
+++ src/views/joinPlan.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="wrap">
+    <div class="">
 
        <mu-appbar style="width: 100%;" color="primary" text-color="#666" z-depth="0" >
         <mu-button icon slot="left" @click="$router.go(-1)">
@@ -7,13 +7,27 @@
         </mu-button>
         <div class="text-center">加入计划</div>
       </mu-appbar>
-      <mu-sub-header>终身重大疾病互助计划</mu-sub-header>
+      <div class="wrap">
+        <mu-sub-header>终身重大疾病互助计划</mu-sub-header>
       <ul class="plan-info">
         <li v-for="(item, index) in 6" :key="index"><span>Lorem, ipsum.</span></li>
       </ul>
       <mu-divider style="height:5px"></mu-divider>
       <div class="info-form">
+          <mu-sub-header>资料填写</mu-sub-header>
+          <div class="input-wrap">
+            <label for="" >姓 名：</label>
+            <input type="text" placeholder="请输入您的姓名">
+          </div>
+           <div class="input-wrap">
+            <label for="" >姓 名：</label>
+            <input type="text" placeholder="请输入您的姓名">
+          </div>
+          <div class="wraning">
 
+            <mu-icon value=""></mu-icon> 
+          </div>
+      </div>
       </div>
     </div>
 </template>
@@ -25,7 +39,9 @@
 </script>
 
 <style scoped lang='scss'>
-
+.wrap{
+  padding: 0 20px;
+}
 ul{
 
     display: flex;
@@ -37,6 +53,13 @@ ul{
     span{
       color: #000;
     }
+  }
+}
+.input-wrap{
+  input{
+    border: none;
+    padding-left: 20px;
+    outline: none;
   }
 }
 </style>
