e336503a28e20a4010e55e7b9d55af27b639ddd9
ywz <1179132719@qq.com>
2019-04-20 11:14:07 +0800

    mycenterUI还原完成

--- src/views/MyCenter.vue
+++ src/views/MyCenter.vue
@@ -17,20 +17,20 @@
          <mu-icon value=":iconfont iconyou1"></mu-icon>
       </div>
       <div class="apply">
-        <mu-icon value=":iconfont iconaixin"></mu-icon>
+        <img src="../assets/图标/爱心.png" alt="">
         <span>申请成为爱心大使</span>
         <div class="apply-click">
           <span>立即申请</span>
           <mu-icon value=":iconfont iconyou"></mu-icon>
         </div>
       </div>
-      <section class="please-list" style="margin: 10px auto; width: 90%">
+      <section class="please-list">
         <router-link tag="div" to="/welfareagency">
           <div class="list-li">
             <div style="text-align: center;">
               <mu-icon value=":iconfont iconfulihui"></mu-icon>
+              <span style="margin-left:12px">福利社</span>
             </div>
-            <span>福利社</span>
             <mu-icon value=":iconfont iconyou1"></mu-icon>
           </div>
         </router-link>
@@ -38,8 +38,8 @@
           <div class="list-li">
             <div style="text-align: center;">
               <mu-icon value=":iconfont iconqiabao"></mu-icon>
+              <span style="margin-left:12px">我的卡包</span>
             </div>
-            <span>我的卡包</span>
             <mu-icon value=":iconfont iconyou1"></mu-icon>
           </div>
         </router-link>
@@ -47,8 +47,8 @@
           <div class="list-li">
             <div style="text-align: center;">
               <mu-icon value=":iconfont iconjihua"></mu-icon>
+              <span style="margin-left:12px">我的计划</span>
             </div>
-            <span>我的计划</span>
             <mu-icon value=":iconfont iconyou1"></mu-icon>
           </div>
         </router-link>
@@ -56,8 +56,8 @@
           <div class="list-li">
             <div style="text-align: center;">
               <mu-icon value=":iconfont iconshenqing"></mu-icon>
+              <span style="margin-left:12px">我的申请</span>
             </div>
-            <span>我的申请</span>
             <mu-icon value=":iconfont iconyou1"></mu-icon>
           </div>
         </router-link>
@@ -65,8 +65,8 @@
           <div class="list-li">
             <div style="text-align: center;">
               <mu-icon value=":iconfont iconkehu"></mu-icon>
+              <span style="margin-left:12px">我的蜂蜜</span>
             </div>
-            <span>我的蜂蜜</span>
             <mu-icon value=":iconfont iconyou1"></mu-icon>
           </div>
         </router-link>
@@ -74,8 +74,8 @@
           <div class="list-li">
             <div style="text-align: center;">
               <mu-icon value=":iconfont iconjilu"></mu-icon>
+              <span style="margin-left:12px">互助记录</span>
             </div>
-            <span>互助记录</span>
             <mu-icon value=":iconfont iconyou1"></mu-icon>
           </div>
         </router-link>
@@ -83,19 +83,19 @@
           <div class="list-li">
             <div style="text-align: center;">
               <mu-icon value=":iconfont iconyaoqing"></mu-icon>
+              <span style="margin-left:12px">邀请好友</span>
             </div>
-            <span>邀请好友</span>
             <mu-icon value=":iconfont iconyou1"></mu-icon>
           </div>
         </router-link>
         </section>
-        <section class="please-list" style="margin: 10px auto; width: 90%">
+        <section class="please-list-tongzhi">
           <router-link tag="div" to="/">
             <div class="list-li">
               <div style="text-align: center;">
                 <mu-icon value=":iconfont icontongzhi"></mu-icon>
+                <span style="margin-left:12px">通知</span>
               </div>
-              <span>通知</span>
               <mu-icon value=":iconfont iconyou1"></mu-icon>
             </div>
           </router-link>
@@ -103,8 +103,8 @@
             <div class="list-li">
               <div style="text-align: center;">
                 <mu-icon value=":iconfont iconshezhi"></mu-icon>
+                <span style="margin-left:12px">设置</span>
               </div>
-              <span>设置</span>
               <mu-icon value=":iconfont iconyou1"></mu-icon>
             </div>
           </router-link>
@@ -255,8 +255,9 @@ width: $gw;
     margin-left: 12px;
     line-height: 12px;
   }
-  .iconaixin{
-    font-size: 49px;
+  img{
+    width: 50px;
+    height: 50px;
     position: relative;
     bottom: 15px;
   }
@@ -287,6 +288,16 @@ width: $gw;
   background:rgba(255,255,255,1);
   box-shadow:0px 0px 3px 0px rgba(0, 0, 0, 0.1);
   border-radius:5px;
+  margin: 20px auto;
+}
+.please-list-tongzhi{
+  width:328px;
+  height:93px;
+  background:rgba(255,255,255,1);
+  box-shadow:0px 0px 3px 0px rgba(0, 0, 0, 0.1);
+  border-radius:5px;
+  margin: 20px auto;
+  margin-top: 40px;
 }
 .list-li {
       height: 50px;
@@ -296,12 +307,13 @@ width: $gw;
       .iconfont {
         color: #358EFD;
         font-size: 20px;
+        margin-left: 14px;
       }
       .iconyou1{
         position: absolute;
         right: 20px;
         font-size: 20px;
-        color: yellow;
+        color: #EF9B1E;
       }
     }
 </style>
