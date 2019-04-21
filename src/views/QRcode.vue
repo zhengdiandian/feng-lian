81f458a241cf4b8b08eb76e00c9ee0dbecbd3ebe
zhengdiandian <zzp19980703@gmail.com>
2019-04-21 11:48:43 +0800

    修改Qrcodevue

--- src/views/QRcode.vue
+++ src/views/QRcode.vue
@@ -22,10 +22,16 @@ export default {
 }
 </script>
 <style scoped lang="scss">
-.card{
+html,body{
     width: 100%;
     height: 100%;
-    background: url("~@/assets/PNG/codebackground.png");
+}
+.card{
+    
+    width: 375px;
+    height: 100%;
+    background: url("../assets/PNG/codebackground.png") center no-repeat;
+    // background-size: 100% ;
     background-size: 100% 100%;
 }
 </style>
