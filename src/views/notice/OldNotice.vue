6a7401d563707e2602ce8eafb140c64ca24427da
zhengdiandian <zzp19980703@gmail.com>
2019-04-21 15:04:15 +0800

    更改navBar

--- src/views/notice/OldNotice.vue
+++ src/views/notice/OldNotice.vue
@@ -1,13 +1,34 @@
 <template>
+  <div>
+    <div class="title">
 
+    </div>
+    <ul>
+      <li>
+        <Card></Card>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+  import Card from "../../components/NoticeCard/NoticeCard";
   export default {
-    name: 'oldNotice'
+    name: 'oldNotice',
+    components:{
+      Card
+    }
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+ul{
+  margin: 0px;
+  padding: 0px;
+  margin-left: 28px;
+}
+  .card{
+    width: 333px;
 
+  }
 </style>
