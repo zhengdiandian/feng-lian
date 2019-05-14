890f26f5a31f3cbf44dc519dac1b1e5a52af45c6
NIcholasYwz <1179132719@qq.com>
2019-05-14 20:19:41 +0800

    add 我的首页互助计划

--- src/views/minePlan.vue
+++ src/views/minePlan.vue
@@ -8,112 +8,150 @@
         <mu-button icon slot="right"  :ripple="false">
         </mu-button>
       </mu-appbar>
+        <div class="page-margin-top"></div>
         <main>
-            <div style="margin: auto;width:92%;">
-                <span class="font">我的购买计划</span>
-                <div  style="margin-top: 8px;">
-                  <!--<section class="card">-->
-                    <!--<card-->
-                    <!--:open="() => {$router.push('/palnned')}"-->
-                    <!--:date="': 2019.01.02'"-->
-                    <!--:waitingperiod="waitingperiod"-->
-                    <!--:waiting="waiting"-->
-                    <!--&gt;-->
-
-                    <!--<template v-slot:lable>-->
-                        <!--<div class="slot-lable">-->
-                            <!--<img src="../assets/img/审核中.svg" alt="">-->
-                        <!--</div>-->
-                    <!--</template>-->
-                    <!--<template v-slot:FilingDate>-->
-                        <!--<div class="dates"><span>申请日：</span>2019.01.01</div>-->
-                    <!--</template>-->
-                    <!--</card>-->
-                    <!--</section>-->
-                    <!--<card-->
-                    <!--:open="() => {$router.push('/palnned')}"-->
-                    <!--:date="': 2019.01.02'"-->
-                    <!--:waitingperiod="waitingperiod"-->
-                    <!--:waiting="waiting"-->
-                    <!--&gt;-->
-
-                    <!--<template v-slot:lable>-->
-                        <!--<div class="slot-lable">-->
-                            <!--<img src="../assets/img/驳回.svg" alt="">-->
-                        <!--</div>-->
-                    <!--</template>-->
-                    <!--<template v-slot:FilingDate>-->
-                        <!--<div class="dates"><span>申请日：</span>2019.01.01</div>-->
-                    <!--</template>-->
-                    <!--<template v-slot:AuditPass>-->
-                      <!--<div class="slot-AuditPass"><img src="../assets/img/审核通过.svg" alt=""></div>-->
-                    <!--</template>-->
-                    <!--</card>-->
-                </div>
+            <!-- <div class="add-family">
+                <span style="font-size:14px;font-family:SourceHanSansCN-Normal;font-weight:bold;color:rgba(51,51,51,1); margin-left: 12px;">我的家人</span>
+                <section class="add-family-list">
+                    <div class="list">
+                        <div class="list-img">
+                            <img src="../assets/PNG/积分头像2.png" alt="">
+                        </div>
+                        <span style="font-size:12px;">{{name}}</span>
+                        <span style="font-size:12px;">自己</span>
+                    </div>
+                    <div class="list">
+                        <div class="list-img">
+                            <img src="../assets/PNG/积分头像2.png" alt="">
+                        </div>
+                        <span style="font-size:12px;">{{name}}</span>
+                        <span style="font-size:12px;">自己</span>
+                    </div>
+                    <div class="list">
+                        <div class="list-img">
+                            <img src="../assets/PNG/积分头像2.png" alt="">
+                        </div>
+                        <span style="font-size:12px;">{{name}}</span>
+                        <span style="font-size:12px;">自己</span>
+                    </div>
+                    <div class="list">
+                        <div class="list-img">
+                            <img src="../assets/PNG/积分头像2.png" alt="">
+                        </div>
+                        <span style="font-size:12px;">{{name}}</span>
+                        <span style="font-size:12px;">自己</span>
+                    </div>
+                    <div class="list">
+                        <div class="list-img">
+                            <img src="../assets/PNG/积分头像2.png" alt="">
+                        </div>
+                        <span style="font-size:12px;">{{name}}</span>
+                        <span style="font-size:12px;">自己</span>
+                    </div>
+                    <div class="list">
+                        <div class="list-img">
+                            <img src="../assets/PNG/添加家人.png" alt="">
+                        </div>
+                        <span style="font-size:12px;">添加家人</span>
+                    </div>
+                </section>
+            </div> -->
+            <div class="purchase-plan" style="margin-top: 50px;">
+                <span  style="font-size:14px;font-family:SourceHanSansCN-Normal;font-weight:bold;color:rgba(51,51,51,1); margin-left: 12px;">我的购买计划</span>
+                <section class="card" style="margin:0;" v-for="(myplan,i) in myplan.list" :key="i">
+                    <card
+                        :open="() => {$router.push({name: 'hlepPlan', params:{productCode: 'PRO201905111234221'}})}"
+                        :img="myplan.headPortrait"
+                        :state="myplan.payState==100?'未实名':'已实名'"
+                        :productName="myplan.productName"
+                        :amount="amount"
+                        :amountMoney="myplan.balance"
+                        :waiting="myplan.leftWattingDays + '天'"
+                        :waitingperiod="waitingperiod"
+                        :date=" ':' +myplan.joinDate"
+                        :name="myplan.contacs"
+                    >
+                    </card>
+                    </section>
             </div>
         </main>
+        <!-- <footerBtn></footerBtn> -->
     </div>
 </template>
 <script>
+import PageHeader from '../components/PageHeader/PageHeader'
+import footerBtn from '../components/footerBtn/FooterBtn'
 import card from '../components/Card/Card'
 export default {
-    name: 'myhelpplan',
+    name: 'myplan',
     components: {
+        PageHeader,
+        footerBtn,
         card
     },
     data() {
         return {
+            amount: '余额',
             waitingperiod: '等待期:',
-            waiting: '180天'
+            name: 'Bytan.zZ',
+            myplan: []
         }
     },
+    mounted() {
+        this.$axios.get('/v1/mutually/plan/planList').then(res=>{
+            console.log(res)
+            this.myplan = res.data.data
+            // console.log(this.myplan)
+        })
+    }
 }
 </script>
 <style scoped lang="scss">
-main{
-    margin: auto;
-    margin-top: 60px;
-    position: relative;
+.card[data-v-6c619236] {
+    margin: -2.8vw auto auto;
 }
 .card{
-  // width: 100%;
 
   height: 180px;
-  display: flex;
-  flex-grow: 1;
 }
-.slot-lable{
-    // width: 100px;
-    // height: 500px;
-    // position: relative;
-    img{
-        position: absolute;
-        right: 0px;
-        top: 16px;
-        width: 50px;
-        height: 50px;
-        z-index: 99;
+span{
+    font-family:SourceHanSansCN-Normal;
+}
+.add-family{
+    margin-bottom: 10px;
+}
+.card{
+    margin: auto;
+    margin-top: 12px;
+}
+.add-family-list{
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 12px;
+    .list{
+    display: flex;
+    flex-direction: column;
+    width: 25%;
+    text-align: center;
+    align-items: center;
+    .list-img{
+        width:50px;
+        height:50px;
+        background:$c-cheng;
+        border:2px solid rgba(239, 162, 32, 1);
+        border-radius:50%;
+        img{
+            width: 100%;
+            height: 100%;
+        }
+        .iconjiahaotianjia{
+            font-size: 25px;
+            position: relative;
+            top: 6px;
+            color: white;
+        }
     }
 }
-.dates{
-        margin-top: 5px;
-        // width:150px;
-        height:22px;
-        background:rgba(246,176,59,1);
-        border-radius:11px;
-        font-size:10px;
-        font-family:SourceHanSansCN-Normal;
-        font-weight:400;
-        text-align: center;
-        line-height: 22px;
-      }
-      .slot-AuditPass{
-        position: absolute;
-        right: -100px;
-        top: 30px;
-        img{
-          width: 72px;
-          height: 54px;;
-        }
-      }
+}
+
 </style>
