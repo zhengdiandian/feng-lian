c3d5565008e267d6b9f6f62681c3d6d6fd37cd06
NIcholasYwz <1179132719@qq.com>
2019-05-18 22:55:54 +0800

    修改字体

--- src/views/Notice.vue
+++ src/views/Notice.vue
@@ -3,7 +3,7 @@
     <PopBox style="bottom: 6.2%">
       <div class="pop-content">
         <div>
-            <div style="color:#f8b62d;margin-bottom:24px;">温馨提示</div>
+            <div style="color:#f8b62d;margin-bottom:24px; font-size: 16px;">温馨提示</div>
           <div>
           </div>
           <div style=" color:#707070;font-size: 14px;">尊敬的蜂链会员您好：</div>
